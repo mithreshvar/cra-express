9390419a3b5cbc60f1d4b3e40892963a44413282
mithreshvar <mithreshvar@gmail.com>
2024-05-06 22:52:20 +0530

    init

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-3xl font-bold underline bg-yellow-500">Hello</h1>
+    </div>
+  );
+}
+
+export default App;
